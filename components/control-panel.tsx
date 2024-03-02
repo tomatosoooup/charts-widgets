5dde3a9d69fb7f63d4647a9ee996b744be8f8799
tomatosoooup <v.gabinet02@icloud.com>
2024-03-02 21:06:48 +0200

    remaking

--- components/control-panel.tsx
+++ components/control-panel.tsx
@@ -1,72 +1,87 @@
 "use client";
 
 import { useState } from "react";
-import { DraggableZone } from "./draggable-zone";
-import { WidgetCard } from "./widget-card";
+import { RiDeleteBin2Fill } from "react-icons/ri";
+
 import { Chart } from "@/types";
 
-const data = [
-  {
-    id: "1",
-    label: "Donut Chart",
-    type: "donut",
-  },
-  {
-    id: "2",
-    label: "Horizontal Chart",
-    type: "horizontal",
-  },
-  {
-    id: "3",
-    label: "Pie Chart",
-    type: "pie",
-  },
-];
+import {DragDropContext, Droppable, Draggable, DropResult} from "@hello-pangea/dnd"
+import { reorder } from "@/lib/utils";
+import { fakeData } from "@/data";
+
+
 
 export const ControlPanel = () => {
-  const [widgets, setWidgets] = useState<Chart[]>([]);
+  const [data, setData] = useState<Chart[]>(fakeData)
 
-  const handleDragStart = (e: React.DragEvent, widgetType: string) => {
-    e.dataTransfer.setData("widgetType", widgetType);
-    console.log("widgetType", widgetType);
-  };
+  const [zoneData, setZoneData] = useState<Chart[]>([])
 
-  const handleDragUpdate = (e: React.DragEvent) => {
-    const widgetType = e.dataTransfer.getData("widgetType") as string;
-    const widget = data.find((item) => `widget-${item.id}` === widgetType);
-    if (widget) {
-      setWidgets((prevWidgets) => [...prevWidgets, widget]);
+  const handleDragStart = ()=> {
+    if(window.navigator.vibrate) window.navigator.vibrate(100);
+  }
+
+  const handleDragEnd = (result: DropResult)=> {
+    if (result.combine) {
+      const newData: Chart[] = [...data];
+      newData.splice(result.source.index, 1);
+      setData(newData);
+      setZoneData((prev) => [...prev, data[result.source.index]]);
+      return;
     }
-  };
+    
+    if (!result.destination) return;
+    
+    if (result.destination.droppableId === "droppable-2") {
+      const item = data[result.source.index];
+      setZoneData((prev) => [...prev, item]);
+    }
+  }
 
-  const handleDragEnd = (e: React.DragEvent) => {
-    e.preventDefault();
+  const handleDeleteItem = (index: number) => {
+    setZoneData(prevZoneData => {
+      const newZoneData = [...prevZoneData];
+      newZoneData.splice(index, 1);
+      return newZoneData;
+    });
   };
+  
 
-  const handleDeleteWidget = (chart_index: number) => {
-    setWidgets((prevWidgets) =>
-      prevWidgets.filter((_, index) => index !== chart_index)
-    );
-  };
+  return (<><div className="flex flex-col gap-5 w-full">
+  <DragDropContext 
+  onDragStart={handleDragStart} 
+  onDragEnd={handleDragEnd}>
+    <Droppable droppableId="droppable">
+    {(droppableProvided)=>(
+      <ul className="grid grid-cols-3 gap-x-5" {...droppableProvided.droppableProps} ref={droppableProvided.innerRef}>
+        {/* {data.map((data, index)=> (
+          <Draggable draggableId={data.id} index={index} key={data.id}>
+            {(draggableProvided)=> (
+              <li className="flex items-center justify-center gap-4 rounded-md bg-default-50 p-4 border" {...draggableProvided.dragHandleProps} {...draggableProvided.draggableProps} ref={draggableProvided.innerRef}>
+                {data.label}
+              </li>
+            )}
+          </Draggable>
+        ))} */}
+        
+        {droppableProvided.placeholder}
+      </ul>
+    )}</Droppable>
+    <Droppable droppableId="droppable-2">
+  {(droppableProvided)=> (
+    <div className="border border-dashed h-[500px] grid grid-cols-3 gap-5 p-5" {...droppableProvided.droppableProps} ref={droppableProvided.innerRef}>{zoneData.map((data, index)=> (
+      <div className="flex flex-col gap-4 rounded-md bg-default-50 p-4 border h-[200px] relative" >
+                <h3 className="uppercase font-semibold">{data.label}</h3>
+                <RiDeleteBin2Fill onClick={() => handleDeleteItem(index)} className="absolute top-2 right-2 w-10 h-10 text-red-600/80 hover:text-red-600/90 cursor-pointer"></RiDeleteBin2Fill>
+              </div>
+    ))}{droppableProvided.placeholder}</div>
+    
+  )}
+  
+</Droppable>
+    </DragDropContext>
+</div>
 
-  return (
-    <section className="flex flex-col w-full gap-y-10">
-      <div className="h-[100px] bg-transparent grid grid-cols-3 gap-x-5">
-        {data.map((data, index) => (
-          <WidgetCard
-            key={index}
-            id={data.id}
-            label={data.label}
-            onDragStart={handleDragStart}
-          />
-        ))}
-      </div>
-      <DraggableZone
-        onDrop={handleDragUpdate}
-        onDragOver={handleDragEnd}
-        onDeleteWidget={handleDeleteWidget}
-        widgets={widgets}
-      />
-    </section>
-  );
+</>)
+  
+  
 };
---
+++ data/index.ts
@@ -0,0 +1,20 @@
+import { Chart } from "@/types";
+
+
+export const fakeData: Chart[] = [
+    {
+      id: "1",
+      label: "Donut Chart",
+      type: "donut",
+    },
+    {
+      id: "2",
+      label: "Horizontal Chart",
+      type: "horizontal",
+    },
+    {
+      id: "3",
+      label: "Pie Chart",
+      type: "pie",
+    },
+  ];
